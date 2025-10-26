baef080d36dbdb933d07ba670f04411c8b44e4a1
Donghwan Oh <imodh97@gmail.com>
2025-10-26 10:56:21 +0900

    test(game): write unit tests for 'Game' class

---
+++ __tests__/GameTest.js
@@ -0,0 +1,27 @@
+import { Game } from '../src/Game';
+import { MissionUtils } from '@woowacourse/mission-utils';
+
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickNumberInRange = jest.fn();
+
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, MissionUtils.Random.pickNumberInRange);
+};
+
+describe('게임 실행', () => {
+  test('올바른 우승자를 리턴하는가?', () => {
+    const carNames = ['pobi', 'woni', 'han'];
+    const trialNumber = 1;
+
+    const MOVING_FORWARD = 4;
+    const STOP = 3;
+
+    mockRandoms([MOVING_FORWARD, STOP, MOVING_FORWARD]);
+
+    const game = new Game(carNames, trialNumber);
+    const winners = game.play();
+
+    expect(winners).toEqual(['pobi', 'han']);
+  });
+});
