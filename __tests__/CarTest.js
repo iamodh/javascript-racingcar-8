3eaa3417a4656279053a3611684a2e84eca301f2
Donghwan Oh <imodh97@gmail.com>
2025-10-26 10:42:24 +0900

    test(car): create car instance for each test instead of for one describe

--- __tests__/CarTest.js
+++ __tests__/CarTest.js
@@ -1,11 +1,11 @@
 import { Car } from '../src/Car';
 
 describe('자동차 기능 테스트', () => {
-  const car = new Car('test');
-
   test('4 미만의 값을 받았을 때 이동하지 않음', () => {
+    const carName = 'pobi';
     const value = 1;
 
+    const car = new Car(carName);
     car.checkAndMove(value);
     const position = car.getPosition();
 
@@ -13,8 +13,10 @@ describe('자동차 기능 테스트', () => {
   });
 
   test('4 이상의 값을 받았을 때 이동', () => {
+    const carName = 'pobi';
     const value = 4;
 
+    const car = new Car(carName);
     car.checkAndMove(value);
     const position = car.getPosition();
 
