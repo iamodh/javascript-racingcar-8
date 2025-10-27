da8f7c7e4667420fdcd69fb3d2f0d30553746dd3
Donghwan Oh <imodh97@gmail.com>
2025-10-27 18:41:17 +0900

    test(input): use parameterized test instead of duplocate conditions

--- __tests__/InputTest.js
+++ __tests__/InputTest.js
@@ -14,17 +14,11 @@ describe('자동차 이름 배열 생성', () => {
     expect(carNames).toEqual(['pobi', 'woni']);
   });
 
-  test('예외 테스트: 5글자 초과인 자동차 이름', () => {
-    const input = 'pobi,javaji';
-
+  test.each([
+    { input: 'pobi,javaji', errorName: '5글자 초과인 자동차 이름' },
+    { input: 'pobi, ', errorName: '빈 문자열 또는 공백의 자동차 이름' },
+  ])('예외 테스트: $errorName', ({ input }) => {
     const action = () => Input.getCarNames(input);
-    expect(action).toThrow('[ERROR]');
-  });
-
-  test('예외 테스트: 빈 문자열 또는 공백의 자동차 이름', () => {
-    const input = 'pobi, ';
-    const action = () => Input.getCarNames(input);
-
     expect(action).toThrow('[ERROR]');
   });
 });
@@ -38,33 +32,12 @@ describe('시도 횟수 생성', () => {
     expect(trialNumber).toEqual(1);
   });
 
-  test('예외 테스트: 숫자로 변환할 수 없는 문자 입력', () => {
-    const input = 'char';
-
-    const action = () => Input.getTrialNumber(input);
-
-    expect(action).toThrow('[ERROR]');
-  });
-
-  test('예외 테스트: 공백 입력', () => {
-    const input = ' ';
-
-    const action = () => Input.getTrialNumber(input);
-
-    expect(action).toThrow('[ERROR]');
-  });
-
-  test('예외 테스트: 음수 입력', () => {
-    const input = '-1';
-
-    const action = () => Input.getTrialNumber(input);
-
-    expect(action).toThrow('[ERROR]');
-  });
-
-  test('예외 테스트: 0 입력', () => {
-    const input = '0';
-
+  test.each([
+    { input: 'char', errorName: '숫자로 변환할 수 없는 문자 입력' },
+    { input: ' ', errorName: '공백 입력' },
+    { input: '-1', errorName: '음수 입력' },
+    { input: '0', errorName: '0 입력' },
+  ])('예외 테스트: $errorName', ({ input }) => {
     const action = () => Input.getTrialNumber(input);
 
     expect(action).toThrow('[ERROR]');
