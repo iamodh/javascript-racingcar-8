f9902108afa8a79dedba71415f3141b23483cc42
Donghwan Oh <imodh97@gmail.com>
2025-10-27 18:59:30 +0900

    test(input): validate maximum limits for car names and trial number

--- __tests__/InputTest.js
+++ __tests__/InputTest.js
@@ -15,8 +15,12 @@ describe('자동차 이름 배열 생성', () => {
   });
 
   test.each([
-    { input: 'pobi,javaji', errorName: '5글자 초과인 자동차 이름' },
-    { input: 'pobi, ', errorName: '빈 문자열 또는 공백의 자동차 이름' },
+    { input: 'pobi,javaji', errorName: '5글자 초과인 자동차 이름 입력' },
+    { input: 'pobi, ', errorName: '빈 문자열 또는 공백의 자동차 이름 입력' },
+    {
+      input: 'a, b, c, d, e, f, g, h, i, j, k',
+      errorName: '너무 많은 자동차 이름 입력',
+    },
   ])('예외 테스트: $errorName', ({ input }) => {
     const action = () => Input.getCarNames(input);
     expect(action).toThrow('[ERROR]');
@@ -37,11 +41,10 @@ describe('시도 횟수 생성', () => {
     { input: ' ', errorName: '공백 입력' },
     { input: '-1', errorName: '음수 입력' },
     { input: '0', errorName: '0 입력' },
+    { input: '1001', errorName: '너무 큰 숫자 입력' },
   ])('예외 테스트: $errorName', ({ input }) => {
     const action = () => Input.getTrialNumber(input);
 
     expect(action).toThrow('[ERROR]');
   });
-
-  //   test('예외 테스트: 너무 큰 숫자 입력');
 });
