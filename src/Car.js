9db71d3424e30a9386919f7f8681d51ae56bd6ca
Donghwan Oh <imodh97@gmail.com>
2025-10-27 14:26:17 +0900

    refactor(car): replace magic numbers with constants

--- src/Car.js
+++ src/Car.js
@@ -1,4 +1,7 @@
 export class Car {
+  #MOVE_DISTANCE = 1;
+  #STANDARD_NUMBER_TO_MOVE = 4;
+
   #name;
   #position = 0;
 
@@ -7,15 +10,20 @@ export class Car {
   }
 
   checkAndMove(randomNumber) {
-    if (randomNumber >= 4) {
-      this.#position += 1;
+    if (randomNumber >= this.#STANDARD_NUMBER_TO_MOVE) {
+      this.#position += this.#MOVE_DISTANCE;
     }
   }
 
   getName() {
     return this.#name;
   }
+
   getPosition() {
     return this.#position;
+  }
+
+  createMovementLog() {
+    return `${this.#name} : ${'-'.repeat(this.#position)}`;
   }
 }
